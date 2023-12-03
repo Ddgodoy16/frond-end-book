9273b16700f9dbb86b8e76afb3e6f1cdb781bc00
DAVID11 <93211987+David11A@users.noreply.github.com>
2023-12-03 17:49:28 -0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import { LogIn } from '../components/LogIn'
 import { InicioLaboratorio } from '../components/InicioLaboratorio'
-import { ReservarLaboratorio } from '../components/ReservarLaboratorio'
-import { ReservarLaboratorio2 } from '../components/ReservarLaboratorio2'
-import { ReservarLaboratorio3 } from '../components/ReservarLaboratorio3'
-import { ReservarLaboratorio4 } from '../components/ReservarLaboratorio4'
-import { ReservarLaboratorio5 } from '../components/ReservarLaboratorio5'
 import { Reservados } from '../components/Reservados'
 import { Registro } from '../components/Registro'
 import { Bitacora } from '../components/Bitacora'
@@ -20,11 +15,6 @@ export const App = () => {
         <Routes>
           <Route path='/' element={<LogIn />}  ></Route>
           <Route path='/inicioLaboratorio' element={<InicioLaboratorio />}  ></Route>
-          <Route path='/reservarLaboratorio' element={<ReservarLaboratorio />}  ></Route>
-          <Route path='/reservarLaboratorio2' element={<ReservarLaboratorio2 />}  ></Route>
-          <Route path='/reservarLaboratorio3' element={<ReservarLaboratorio3 />}  ></Route>
-          <Route path='/reservarLaboratorio4' element={<ReservarLaboratorio4 />}  ></Route>
-          <Route path='/reservarLaboratorio5' element={<ReservarLaboratorio5 />}  ></Route>
           <Route path='/reservados' element={<Reservados />}  ></Route>
           <Route path='/registro' element={<Registro />}  ></Route>
           <Route path='/bitacora' element={<Bitacora />}  ></Route>
